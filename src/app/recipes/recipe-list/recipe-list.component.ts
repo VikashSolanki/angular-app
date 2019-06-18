065eebe3a86f9923ca78a5e0d7e8d866bf7551fe
vikash solanki <vikram@techuz.com>
2019-06-18 15:32:25 +0530

    Add html for recipe and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
+import { from } from 'rxjs';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A text Recipe',"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",'https://cdn.pixabay.com/photo/2018/08/30/10/22/plum-cake-3641833_960_720.jpg'),
+    new Recipe('A text Recipe','The description','https://cdn.pixabay.com/photo/2018/08/30/10/22/plum-cake-3641833_960_720.jpg'),
+    new Recipe('A text Recipe','The description','https://cdn.pixabay.com/photo/2018/08/30/10/22/plum-cake-3641833_960_720.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredients } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients("Apple",10),
+    new Ingredients("Potato",20)
+  ];
 
   constructor() { }
 
